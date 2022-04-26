6587191b4d9070182af4c77398460a56ac146e2f
Fernando Passos <fernandopassoz@gmail.com>
2022-04-26 20:47:27 -0300

    elixir process list

--- elixir/second.exs
+++ elixir/second.exs
@@ -1,11 +1,23 @@
-System.cmd("ps", ["aux"])
-|> Tuple.to_list()
-|> List.first()
-|> String.split("\n")
-|> Enum.filter(fn el -> Regex.match?(~r/firefox/, el) end)
-|> Enum.map(fn el ->
-  String.split(el, " ", trim: true)
-  |> IO.inspect()
+defmodule Second do
+  def exec() do
+    System.cmd("ps", ["aux"])
+    |> Tuple.to_list()
+    |> List.first()
+    |> String.split("\n")
+    |> Enum.filter(fn el -> Regex.match?(~r/firefox/, el) end)
+    |> Enum.map(fn el ->
+      String.split(el, " ", trim: true)
+      |> process_list()
+      |> IO.puts()
+    end)
+  end
 
-  # |> Enum.at(el, 0)
-end)
+  defp process_list(list) when is_list(list) and list > 3 do
+    # [head | tail] = list
+    # IO.puts(head)
+    # process_list(tail)
+    %{user: Enum.fetch!(list, 0), pid: Enum.fetch!(list, 1)}
+  end
+end
+
+Second.exec()

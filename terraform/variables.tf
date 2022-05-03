c35bc951f1fa0ab40bfab996af8f6ecf60116ec3
Fernando Passos <fernandopassoz@gmail.com>
2022-05-03 17:20:00 -0300

    terraform machine 1 ok plus kube installed

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "volume-size" {
+    type = number
+    default = 10 * 1024 * 1024 * 1024
+}
+
+variable "volume-source-local" {
+    type = string
+    default = "/home/fernando/Files/VMs/jammy-server-cloudimg-amd64.img"
+}
+
+variable "volume-source-remote" {
+    type = string
+    default = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
+
+variable "network-name" {
+    type = string
+    default = "default"
+}
+  

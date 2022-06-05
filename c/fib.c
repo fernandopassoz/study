1a9b826efd8d3fde35555ad9b41589e4f04aacaa
Fernando Passos <fernandopassoz@gmail.com>
2022-06-05 11:33:14 -0300

    c crazy horse fib

--- c/fib.c
+++ c/fib.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int a = 0, b = 1, o = 0, max = 10;
 
-
-void fib(int a, int b){
+void fib(int a, int b) {
   o = a;
   a = b;
   b = a + o;
-  if(a <= max){
+  if (a <= max) {
     printf("%d\n", a);
     fib(a, b);
   }
 }
 
-void main(int argc, char *argv[]){
-  max = atoi(argv[1]);
-  printf("%d\n", max);
-  //fib(a, b);
+int main(int argc, const char **argv) {
+  if (argc == 1) {
+    max = 0;
+  } else {
+    max = atoi(argv[1]);
+  }
+  fib(a, b);
+
+  return 0;
 }
